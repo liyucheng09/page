689a69b39c4ee626843d3f78756007f6205768ac
Rulin Shao <Srl0310@outlook.com>
2023-05-16 18:04:48 -0700

    Update utils.js

--- utils.js
+++ utils.js
@@ -13,7 +13,7 @@ function createProjectElement(id, project){
     if (project.others == null)
         project.others = "";
 
-    html_img = `<img src='${project.image}' style="max-width: 160px"></div></div>`
+    html_img = `<img src='${project.image}' style="max-width: 100px"></div></div>`
     html_txt = `<p>
       <a href="${project.paper_url}"><papertitle>${project.title}</papertitle></a>
       <br>
