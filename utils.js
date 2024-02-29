a63640fc1ec1a7e6100472c6d148c84d135d2ec4
liyucheng <liyucheng_2@qq.com>
2024-02-29 21:46:29 +0000

    add demo

--- utils.js
+++ utils.js
@@ -17,13 +17,14 @@ function createProjectElement(id, project) {
     var html_img = `<img src='${project.image}' style="max-width: 180px"></div></div>`;
     var codeLink = project.code ? `<a href="${project.code}">[code]</a>` : "";
     var zhihu = project.zhihu ? `<a href="${project.zhihu}">[知乎]</a>` : "";
+    var demo = project.demo ? `<a href="${project.demo}">[demo]</a>` : "";
     
     var html_txt = `<p>
       <a href="${project.paper_url}"><papertitle>${project.title}</papertitle></a>
       <br>
       ${project.authors}
       <br>
-      <em>${project.conference}</em> &nbsp; ${codeLink} &nbsp; ${zhihu}
+      <em>${project.conference}</em> &nbsp; ${codeLink} &nbsp; ${zhihu} &nbsp; ${demo}
       <br>
       ${project.others}`
 
